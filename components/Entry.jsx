02834013d20168bb98bd0e96428022cc2ace8a38
Milos_Petrovic <milos.petrovic@shindiristudio.com>
2025-08-11 10:17:18 +0200

    understanding props

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,32 +1,18 @@
 import "./entry.css";
 
-export default function Entry() {
+export default function Entry(props) {
   return (
-    <main>
-      <section className="section1">
-        <img
-          src="/images/mount_fuji.jpg"
-          alt="Mount Fuji"
-          className="japanImg"
-        />
-        <img src="/images/marker.png" alt="pointy marker" className="marker" />
-        <div>
-          <span className="japanText">Japan</span>
-          <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
-            View on Google Maps
-          </a>
-          <h1 className="nameLabel">Mount Fuji</h1>
-          <br />
-          <span>12 Jan, 2021 - 24 Jan, 2021</span>
-          <p>
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776{" "}
-            <br />
-            meters (12,380 feet). Mount Fuji is the single most popular tourist{" "}
-            <br />
-            site in Japan, for both Japanese and foreign tourists.
-          </p>
-        </div>
-      </section>
-    </main>
+    <section className="section1">
+      <img src={props.img.src} alt={props.img.alt} className="japanImg" />
+      <img src="./images/marker.png" alt="pointy marker" className="marker" />
+      <div>
+        <span className="japanText">{props.country}</span>
+        <a href={props.googleMapsLink}>View on Google Maps</a>
+        <h1 className="nameLabel">{props.title}</h1>
+        <br />
+        <span>{props.dates}</span>
+        <p>{props.text}</p>
+      </div>
+    </section>
   );
 }
